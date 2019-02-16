83125055feeb4a1ca3f56f12992edb8e3ba67e83
LannyWang009 <09legend.wang@gmail.com>
2019-02-16 11:46:44 -0600

    use this.state without redux

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-ReactDOM.render(<div>implement a counter and render it here!</div>, document.getElementById('root'))
+// import { prependOnceListener } from 'cluster'
+
+// let MyComponent = (props) => {
+//   return <div>Hello {props.name}</div>
+// }
+
+// ReactDOM.render(<MyComponent name={'banana'} />, document.getElementById('root'))
+class MyComponent extends React.Component {
+  constructor (props) {
+    console.log('this is the constructor running.')
+    super(props)
+    this.state = {
+      value: 0
+    }
+  }
+  componentDidMount () {
+    console.log('My component has landed!')
+  }
+  componentDidUpdate () {
+    console.log('This component has changes in props or state')
+  }
+  componentWillUnmount () {
+    console.log('Component is being removed from the DOM')
+  }
+
+  handleClick = () => {
+    console.log('hi, i am just clicked', 'this', this)
+    this.setState({
+      value: this.state.value + 1
+    })
+  }
+
+  render () {
+    console.log('this render has been called', 'this.props', this.props)
+    return <div>
+        Hello, {this.props.name}
+      <br />
+      {this.state.value}
+      <button onClick={this.handleClick} value={this.state.value}>Button</button>
+
+    </div>
+  }
+}
+
+ReactDOM.render(<MyComponent name={'banana'} />, document.getElementById('root'))
